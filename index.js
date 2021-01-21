33e003b8c4e57a6dc9ca697ef4927201902b9dcf
Andrew Sobottka <andrewsobottka@Andrews-MacBook-Pro-2.local>
2021-01-21 13:16:55 -0400

    separate approval function from index.js; add details to tokenData.js; update package.json

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 const inputs = require('./YD-ETH-MAR21-inputs.js')
 const approve = require('./approve.js')
 const jsonData = require('./tokenData.json')
-const approvalStatus = require('./approvalStatus.json')
 
 // SERVER CONFIG
 const PORT = process.env.PORT || 5000
@@ -56,49 +55,36 @@ async function walletBalance() {
         // Check balance of USDC
         balance = await baseToken.methods.balanceOf(inputs.baseTokenAccount).call()
         balance = web3.utils.fromWei(balance.toString(), 'mwei')
-        console.log('USDC Balance in Wallet: ', balance)
+        console.log(baseTokenData.symbol, 'balance in Wallet: ', balance)
         // Check balance of synthetic
         balance = await targetToken.methods.balanceOf(inputs.baseTokenAccount).call()
         balance = web3.utils.fromWei(balance.toString(), 'ether')
-        console.log('Synth Balance in Wallet: ', balance)
+        console.log(targetTokenData.symbol, 'balance in Wallet: ', balance)
     } catch (error) {
         console.error(`Could not get balance: ${error}`)
     }
 }
 
-
-/* async function approveToken(tokenInstance, receiver, amount) {
-    try {
-        let approval = await tokenInstance.methods.approve(receiver, amount).send({ from: inputs.baseTokenAccount })
-        console.log(`ERC20 token approved: tx/${approval.transactionHash}`)
-        return amount
-
-    } catch (error) {
-        console.log('ERC20 could not be approved')
-        console.error(`Error approving token: ${error}`)
-    }   
-} */
-
-async function usdcForSynthExchange() {
+async function tokenExchange() {
     try {        
-        /***** current pool status *****/
+        /***** Pull current pool variables for subsequent calculations *****/
         baseTokenBalance = await contract.methods.getBalance(baseTokenData.address).call()
         targetTokenBalance = await contract.methods.getBalance(targetTokenData.address).call()
         targetSpotPrice = await contract.methods.getSpotPrice(baseTokenData.address, targetTokenData.address).call()
         poolFee = await contract.methods.getSwapFee().call()
         
-        /***** max amount we can trade based on impact to spot price *****/
+        /***** Calculate max amount we can trade based on impact to spot price *****/
         inGivenPrice = baseTokenBalance.toString() * (((targetTokenValue / targetSpotPrice.toString()) ** inputs.weightRatio) - 1)
         inGivenPrice = Math.floor(inGivenPrice)
         tradeSize = Math.min(inGivenPrice, maxTrade)
         
-        /***** slippage *****/
+        /***** Set slippage *****/
         outGivenIn = await contract.methods.calcOutGivenIn(baseTokenBalance, weight, targetTokenBalance, weight, tradeSize.toString(), poolFee).call()
         minSynthOut = web3.utils.toBN(outGivenIn.toString() * 0.99) // 1% acceptable slippage
         maxPrice = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
-        console.log('Min Synths to be Received: ', minSynthOut.toString())
+        //console.log('Min Synths to be Received: ', minSynthOut.toString())
 
-        /***** gas limit and price *****/
+        /***** Estimate gas limit and price *****/
         var gasLimit = await contract.methods.swapExactAmountIn(baseTokenData.address, tradeSize.toString(), targetTokenData.address, minSynthOut.toString(), maxPrice).estimateGas({from: inputs.baseTokenAccount})
         var gasPrice = await web3.eth.getGasPrice()
         gasPrice = web3.utils.toBN(gasPrice * 1.10) // will pay 10% above current avg. gas prices to expedite transaction
@@ -111,10 +97,9 @@ async function usdcForSynthExchange() {
             transactionConfirmationBlocks: 1
         }
         
-        console.log('Gas Price Set: ', web3.utils.fromWei(gasPrice.toString(), 'gwei'))
-        console.log('Trade Size: ', web3.utils.fromWei(tradeSize.toString(), 'mwei'), 'USDC for approx:', web3.utils.fromWei(outGivenIn.toString(), 'ether'), 'target tokens')
+        //console.log('Gas Price Set: ', web3.utils.fromWei(gasPrice.toString(), 'gwei'))
+        console.log('Trading: ', web3.utils.fromWei(tradeSize.toString(), 'mwei'), 'USDC for approx:', web3.utils.fromWei(outGivenIn.toString(), 'ether'), 'target tokens')
         
-        console.log('Performing swap...')
         let result = await contract.methods.swapExactAmountIn(baseTokenData.address, tradeSize.toString(), targetTokenData.address, minSynthOut.toString(), maxPrice).send(SETTINGS)
         console.log('Swap Successful! tx/', result.transactionHash)
 
@@ -131,35 +116,53 @@ async function monitorPrice() {
     if (monitoringPrice) {
         return
     }
+    
+    console.log('--- Refreshing ---')
 
-    approvedAmount = approvalStatus['approvedAmount']
-    if (approvedAmount < maxTrade) {
-        approvedAmount = await approve.approveToken(baseToken, contractData.address, baseTokenApproved, inputs.baseTokenAccount)
-        console.log('Additional USDC Approved: ', approvedAmount)
+    /***** ERC20 Token Approval *****/
+    /* The current amt of tokens approved to trade is stored in the approvalStatus.json
+        file and referenced here. When the current amount approved is estimated below
+        the max trade size, an additional approval request will be submitted. The amt
+        approved will be maintained evevn after program is terminated. */
+    approvalStatus = JSON.parse(fs.readFileSync('approvalStatus.json'))
+    currApprovedAmount = approvalStatus.approvedAmount
+    //console.log('Amount already approved: ', currApprovedAmount.toString())
+    
+    if (currApprovedAmount <= maxTrade) {
+        currApprovedAmount = await approve.approveToken(baseToken, contractData.address, baseTokenApproved, inputs.baseTokenAccount)
+        console.log('Additional USDC Approved: ', currApprovedAmount)
+        var newApprovedAmount = { approvedAmount: currApprovedAmount}
+        fs.writeFileSync('./approvalStatus.json', JSON.stringify(newApprovedAmount, null, 2) , 'utf-8');
     }
-    approvedAmount = approvedAmount - maxTrade
-    fs.writeFileSync('./approvalStatus.json', JSON.stringify(approvedAmount, null, 2) , 'utf-8');
 
+    /***** Swap *****/
     console.log('Checking price...')
     monitoringPrice = true
 
     try {
+        /***** Checking Price *****/
+        /* Swap is only performed if current Spot Price is below the target price. */
         synthSpotPrice = await contract.methods.getSpotPrice(baseTokenData.address, targetTokenData.address).call()
         price = web3.utils.fromWei(synthSpotPrice.toString(), 'mwei')
-        console.log('Current Price: ', price, 'USDC')
-
+        console.log('Current Price: 1', targetTokenData.symbol, 'for', price, 'USDC')
         let currentPrice = Number(price)
         let targetPrice = Number(inputs.targetTokenLimit)
         if(currentPrice < targetPrice) {
-            console.log('Buying synth...')
-            
-            await walletBalance()
+            console.log(`Price is below target of ${targetPrice.toString()}!`,'Executing Swap...')
 
-            await usdcForSynthExchange()
+            await tokenExchange()
 
             await walletBalance()
 
-            //clearInterval(priceMonitor)
+            /***** Update Approval Counter *****/
+            /* Approval is reduced by maxTrade size, regardless of actual trade size 
+                and written to the approvalStatus.json file so that the running approval
+                amount will be maintained after program termination. */
+            currApprovedAmount = currApprovedAmount - maxTrade
+            var newApprovedAmount = { approvedAmount: currApprovedAmount}
+            fs.writeFileSync('./approvalStatus.json', JSON.stringify(newApprovedAmount, null, 2) , 'utf-8');
+
+            console.log('--- Swap Complete ---')
         }
 
     } catch (error) {
@@ -172,5 +175,5 @@ async function monitorPrice() {
 }
 
 // Checks pool every n seconds
-const POLLING_INTERVAL = process.env.POLLING_INTERVAL || 5000 // 5 Seconds
+const POLLING_INTERVAL = process.env.POLLING_INTERVAL || 4000 // 4 Seconds
 priceMonitor = setInterval(async () => { await monitorPrice() }, POLLING_INTERVAL)
